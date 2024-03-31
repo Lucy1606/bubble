16ce46182ebe8f99af2fb27fda0c5112ca21d486
Lucy1606 <165541614+Lucy1606@users.noreply.github.com>
2024-03-31 02:56:55 -0300

    Add files via upload

---
+++ Bubblepopper.js
@@ -0,0 +1,18 @@
+setInterval(function() {
+  document.getElementById("BubblePopperCost").innerText = abbrNum(BubblePopperCost,3);
+}, 100);
+setInterval(function() {
+  document.getElementById("PopperWaver1Cost").innerText = abbrNum(PopperWaver1Cost,3);
+}, 100);
+setInterval(function() {
+  document.getElementById("bubblepopperbps").innerText = abbrNum(bubblepopperbps,3);
+}, 100);
+setInterval(function() {
+  document.getElementById("bubbleClick").innerText = abbrNum(bubbleClick,3);
+}, 100);
+setInterval(function() {
+  document.getElementById("ClickBoost").innerText = ClickBoost;
+}, 100);
+setInterval(function() {
+  document.getElementById("BuoIPrice").innerText = abbrNum(BuoIPrice,3);
+}, 100);
