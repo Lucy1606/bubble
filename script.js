2e46dfb813040e98525b1cccfb5b3c8eb4b227ae
Lucy1606 <165541614+Lucy1606@users.noreply.github.com>
2024-03-31 03:07:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-let x = 0;
-let bubbleClick = 1;
-let bubbletotalClicks = 0;
+let X = 0;
+let BubbleClick = 1;
+let BubbletotalClicks = 0;
 let ClickBoost = 1.00;
-let bubblepopper = 0;
+let Bubblepopper = 0;
 let BubblePopperCost = 25;
 let PopperWaver1Cost = 100;
 let BuoIPrice = 1e9;
-let bubblepopperbps = 0;
+let Bubblepopperbps = 0;
 let PopperWaver1Bought = 0;
 let Bubblespersecond = bubblepopperbps;
 let PlasmaBubbles = 0;
@@ -15,8 +15,8 @@ let BuoI = 0
 let BuoIBoost = 1
 
 function popBubble() {
-  x = x + Math.round(bubbleClick * 10) / 10;
-  bubbletotalClicks = bubbletotalClicks + 1;
+  X = X + Math.round(BubbleClick * 10) / 10;
+  BubbletotalClicks = BubbletotalClicks + 1;
   if (ClickBoost < 10) {
     ClickBoost = Math.floor((ClickBoost * 1000) * 1.0025285) / 1000;
   }
@@ -24,21 +24,21 @@ function popBubble() {
     ClickBoost = 10
   }
   setInterval(function() {
-    document.getElementById("B").innerHTML = Math.round(x)
-    document.getElementById("B").innerText = abbrNum(x,3)
-    document.getElementById("bubbletotalClicks").innerText = bubbletotalClicks;
+    document.getElementById("B").innerHTML = Math.round(X)
+    document.getElementById("B").innerText = abbrNum(X,3)
+    document.getElementById("BubbletotalClicks").innerText = BubbletotalClicks;
   }, 1000);
 }
 function buyBubblePopper() {
-  if (x >= BubblePopperCost) { 
-    x = x - BubblePopperCost;
-    bubblepopper = bubblepopper + 1;
-    Bubblespersecond = Math.ceil(bubblepopperbps);
-    document.getElementById("bubblepopper").innerHTML = bubblepopper;
-    document.getElementById("bubblepopperbps").innerHTML = bubblepopperbps;
-    document.getElementById("bubblepopperbps").innerText = abbrNum(bubblepopperbps,3)
+  if (X >= BubblePopperCost) { 
+    X = X - BubblePopperCost;
+    Bubblepopper = Bubblepopper + 1;
+    Bubblespersecond = Math.ceil(Bubblepopperbps);
+    document.getElementById("Bubblepopper").innerHTML = Bubblepopper;
+    document.getElementById("Bubblepopperbps").innerHTML = Bubblepopperbps;
+    document.getElementById("Bubblepopperbps").innerText = abbrNum(Bubblepopperbps,3)
     BubblePopperCost = Math.ceil(BubblePopperCost * 1.15);
-    BubblePopperCost = Math.ceil(BubblePopperCost * Math.pow(1.0145, (bubblepopper + 1)));
+    BubblePopperCost = Math.ceil(BubblePopperCost * Math.pow(1.0145, (Bubblepopper + 1)));
     document.getElementById("BubblePopperCost").innerHTML = BubblePopperCost;
     document.getElementById("BubblePopperCost").innerText = abbrNum(BubblePopperCost,3)
   }
@@ -46,16 +46,16 @@ function buyBubblePopper() {
 
 
  setInterval(function() {
-  x = Math.round(x + ((bubblepopperbps)));
+  X = Math.round(X + ((Bubblepopperbps)));
  }, 1000);
 
 
 function PopperWaver1() {
-  if (x >= PopperWaver1Cost) { 
-    x = x - PopperWaver1Cost;    
+  if (X >= PopperWaver1Cost) { 
+    X = X - PopperWaver1Cost;    
     PopperWaver1Cost = Math.ceil(PopperWaver1Cost * 2);
     PopperWaver1Cost = Math.ceil(PopperWaver1Cost * Math.pow(1.025, (PopperWaver1Bought + 1)));
-    Bubblespersecond = Math.ceil(bubblepopperbps);
+    Bubblespersecond = Math.ceil(Bubblepopperbps);
     PopperWaver1Bought = PopperWaver1Bought + 1;
     document.getElementById("PopperWaver1Cost").innerHTML = PopperWaver1Cost;
     document.getElementById("PopperWaver1Cost").innerText = abbrNum(PopperWaver1Cost,3)
@@ -64,36 +64,36 @@ function PopperWaver1() {
 }
 
 function BuoyancyI() {
-  if (x >= BuoIPrice) {
-    x = x - 1e9;
+  if (X >= BuoIPrice) {
+    X = X - 1e9;
     BuoI = 1;
     BuoIPrice = NaN;
-    BuoIBoost = 10 * (bubblepopper / 10);
+    BuoIBoost = 10 * (Bubblepopper / 10);
   }
 }
-if (x > 1e12) {
+if (X > 1e12) {
   document.getElementById("LogBubbles").innerHTML = LogBubbles
 }
-if (x < 1e12) {
+if (X < 1e12) {
   LogBubbles = 0
 }
 function Bloat() {
-  if (x >= 1e12) {
-    x = 0
-    bubblepopper = 0
+  if (X >= 1e12) {
+    X = 0
+    Bubblepopper = 0
     PopperWaver1Bought = 0
     ClickBoost = 1.00
     BubblePopperCost = 25
     PopperWaver1Cost = 100
-    bubbletotalClicks = 0
-    bubbleClick = 1
+    BubbletotalClicks = 0
+    BubbleClick = 1
     PlasmaBubbles = PlasmaBubbles + Math.floor(Math.pow(bubbles, 0.1))
     
   }
 }
 
 setInterval(function() {
-  bubblepopperbps= Math.floor(ClickBoost * bubblepopper * (1 * Math.pow(1.6, PopperWaver1Bought)));
+  Bubblepopperbps= Math.floor(ClickBoost * bubblepopper * (1 * Math.pow(1.6, PopperWaver1Bought)));
  }, 30);
 
 setInterval(function() {
@@ -106,10 +106,10 @@ setInterval(function() {
   document.getElementById("PopperWaver1Cost").innerText = abbrNum(PopperWaver1Cost,3);
 }, 100);
 setInterval(function() {
-  document.getElementById("bubblepopperbps").innerText = abbrNum(bubblepopperbps,3);
+  document.getElementById("bubblepopperbps").innerText = abbrNum(Bubblepopperbps,3);
 }, 100);
 setInterval(function() {
-  document.getElementById("bubbleClick").innerText = abbrNum(bubbleClick,3);
+  document.getElementById("bubbleClick").innerText = abbrNum(BubbleClick,3);
 }, 100);
 setInterval(function() {
   document.getElementById("ClickBoost").innerText = ClickBoost;
